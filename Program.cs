f8cf69e5a97045af07a72f2b23752d0d3f3bcd70
prodbuild <shamajit@gmail.com>
2016-01-17 15:29:03 +0530

    ChangesDone

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ADODotNetFeatures
 {
@@ -10,11 +8,11 @@ namespace ADODotNetFeatures
     {
         static void Main(string[] args)
         {
-            // AddCustomer();
+            AddCustomer();
 
             AddNewCustomer();
 
-            // GetAllCustomer();
+            GetAllCustomer();
 
             GetAllCustomerAndOrder();
 
